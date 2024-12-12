eaa9ac56069296486a03b7935e22930c5bcb9bcf
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2024-12-12 18:53:46 +0100

    types.kt: error code and fix compilation

--- src/main/kotlin/types.util.kt
+++ src/main/kotlin/types.util.kt
@@ -1,6 +1,3 @@
-import Request.Method
-import Request.Method.Defined
-import Request.Method.Unknown
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.descriptors.PrimitiveKind
@@ -9,9 +6,25 @@ import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
+internal object ErrorCodeSerializer : KSerializer<ErrorCode> {
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("ErrorCode", PrimitiveKind.STRING)
+
+    @OptIn(ExperimentalSerializationApi::class)
+    override fun serialize(encoder: Encoder, value: ErrorCode) {
+        encoder.encodeInt(value.code)
+    }
+
+    override fun deserialize(decoder: Decoder): ErrorCode {
+        val decodedString = decoder.decodeInt()
+        return ErrorCode.Defined.entries.firstOrNull { it.code == decodedString }
+            ?: ErrorCode.Unknown(decodedString)
+    }
+}
+
 internal object RequestMethodSerializer : KSerializer<Method> {
     override val descriptor: SerialDescriptor =
-        PrimitiveSerialDescriptor("Request\$Method", PrimitiveKind.STRING)
+        PrimitiveSerialDescriptor("Method", PrimitiveKind.STRING)
 
     @OptIn(ExperimentalSerializationApi::class)
     override fun serialize(encoder: Encoder, value: Method) {
@@ -20,7 +33,7 @@ internal object RequestMethodSerializer : KSerializer<Method> {
 
     override fun deserialize(decoder: Decoder): Method {
         val decodedString = decoder.decodeString()
-        return Defined.entries.firstOrNull { it.value == decodedString }
-            ?: Unknown(decodedString)
+        return Method.Defined.entries.firstOrNull { it.value == decodedString }
+            ?: Method.Unknown(decodedString)
     }
 }
