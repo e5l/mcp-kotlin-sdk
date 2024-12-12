5ab727837c6a3801b932dcc0fff0064522c88bd8
leonid.stashevsky <leonid.stashevsky@gmail.com>
2024-12-12 15:55:44 +0100

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.21"
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 group = "org.jetbrains.mcp"
@@ -10,6 +11,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
     testImplementation(kotlin("test"))
 }
 
