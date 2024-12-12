06e2a2af11c8028b2f9a111413e45efbba325cfb
leonid.stashevsky <leonid.stashevsky@gmail.com>
2024-12-12 16:21:33 +0100

    Add stdio

---
+++ src/main/kotlin/shared/Stdio.kt
@@ -0,0 +1,41 @@
+package shared
+
+import JSONRPCMessage
+import io.ktor.utils.io.core.append
+import io.ktor.utils.io.core.isEmpty
+import io.ktor.utils.io.core.writeFully
+import kotlinx.io.Buffer
+import kotlinx.io.readLine
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+
+/**
+ * Buffers a continuous stdio stream into discrete JSON-RPC messages.
+ */
+class ReadBuffer {
+    private val buffer: Buffer = Buffer()
+
+    fun append(chunk: ByteArray) {
+        buffer.writeFully(chunk)
+    }
+
+    fun readMessage(): JSONRPCMessage? {
+        if (buffer.exhausted()) return null
+        val line = buffer.readLine() ?: return null
+        return deserializeMessage(line)
+    }
+
+    fun clear() {
+        buffer.clear()
+    }
+}
+
+fun deserializeMessage(line: String): JSONRPCMessage {
+    return Json.decodeFromString(line)
+}
+
+fun serializeMessage(message: JSONRPCMessage): String {
+    return Json.encodeToString(message) + "\n"
+}
+
