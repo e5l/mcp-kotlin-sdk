73a60e037add198550256da9917d05fd3bd039df
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2024-12-12 16:13:49 +0100

    types.kt

--- src/main/kotlin/types.kt
+++ src/main/kotlin/types.kt
@@ -1,3 +1,65 @@
+const val LATEST_PROTOCOL_VERSION = "2024-11-05"
+
+val SUPPORTED_PROTOCOL_VERSIONS = arrayOf(
+    LATEST_PROTOCOL_VERSION,
+    "2024-10-07",
+)
+
+const val JSONRPC_VERSION = "2.0"
+
+// line 15
+/**
+ * A progress token, used to associate progress notifications with the original request.
+ */
+typealias ProgressTokenSchema = Any
+
+/**
+ * An opaque token used to represent a cursor for pagination.
+ */
+typealias CursorSchema = String
+
+interface PassthroughObject {
+    val additionalProperties: Map<String, Any?>
+}
+
+interface WithMeta {
+    /**
+     * This result property is reserved by the protocol to allow clients and servers to attach additional metadata to their responses.
+     */
+    val _meta: PassthroughObject?
+}
+
+interface BaseRequestParamsSchema : PassthroughObject, WithMeta {
+    override val _meta: Meta?
+
+    interface Meta : PassthroughObject {
+        /**
+         * If specified, the caller is requesting out-of-band progress notifications for this request (as represented by notifications/progress). The value of this parameter is an opaque token that will be attached to any subsequent notifications. The receiver is not obligated to provide these notifications.
+         */
+        val progressToken: ProgressTokenSchema?
+    }
+}
+
+interface RequestSchema {
+    val method: String
+    val params: BaseRequestParamsSchema?
+}
+
+interface BaseNotificationParamsSchema : PassthroughObject, WithMeta
+
+
+interface NotificationSchema : PassthroughObject {
+    val method: String
+    val params: BaseNotificationParamsSchema?
+}
+
+interface ResultSchema : PassthroughObject, WithMeta
+
+/**
+ * A uniquely identifying ID for a request in JSON-RPC.
+ */
+typealias RequestIdSchema = Any
+
 
 typealias JSONRPCMessage = JSONRPCMessageSchema
 
