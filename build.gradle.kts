afbc8fc881a12194ca08fd9abdea0234e195da2e
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2024-12-13 22:05:54 +0100

    added websocket server

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
     implementation("io.ktor:ktor-client-apache:3.0.2")
     implementation("io.ktor:ktor-server-sse:3.0.2")
+    implementation("io.ktor:ktor-server-websockets:3.0.2")
     testImplementation(kotlin("test"))
     testImplementation("io.mockk:mockk:1.13.13")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
